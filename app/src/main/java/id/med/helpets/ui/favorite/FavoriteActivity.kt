3ff18da422fbcbae6de827cec65897c0a7ec36c7
Zetaaa <113325074+Zetaaa16@users.noreply.github.com>
2022-11-29 20:05:27 +0700

    test

---
+++ app/src/main/java/id/med/helpets/ui/favorite/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package id.med.helpets.ui.favorite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.med.helpets.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
