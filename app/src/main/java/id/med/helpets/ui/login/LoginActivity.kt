cb5c4ddbea8751c912006822d344234dc5df8b91
SriYanisaa <sriyanisa03@gmail.com>
2022-11-30 21:56:53 +0800

    menambahkan bagian halaman login serta custom view dari login, mengganti tema aplikasi

---
+++ app/src/main/java/id/med/helpets/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package id.med.helpets.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.med.helpets.R
+import id.med.helpets.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
