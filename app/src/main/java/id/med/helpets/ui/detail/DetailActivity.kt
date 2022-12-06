54563fa06ff79b0d70a3f1108f44f36ebbe64c40
Medhiko <mbirajabiraja@gmail.com>
2022-12-06 20:29:44 +0800

    make detail activity design

---
+++ app/src/main/java/id/med/helpets/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package id.med.helpets.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.med.helpets.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
