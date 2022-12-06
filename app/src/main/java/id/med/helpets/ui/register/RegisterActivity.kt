c8351bf7f2f9c4028940d9be8597181ad49bd748
SriYanisaa <sriyanisa03@gmail.com>
2022-12-06 14:52:44 +0800

    Menambahkan authentication firebase agar bisa login dan register

--- app/src/main/java/id/med/helpets/ui/register/RegisterActivity.kt
+++ app/src/main/java/id/med/helpets/ui/register/RegisterActivity.kt
@@ -1,12 +1,64 @@
 package id.med.helpets.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import id.med.helpets.R
+import id.med.helpets.databinding.ActivityRegisterBinding
+import id.med.helpets.ui.login.LoginActivity
+import id.med.helpets.ui.main.MainActivity
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        auth = Firebase.auth
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+            Register(email, password)
+        }
+
+    }
+
+    private fun Register(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful){
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(baseContext, "Berhasil", Toast.LENGTH_SHORT).show()
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?){
+        if (currentUser != null){
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+    companion object{
+        private const val TAG = "RegisterActivity"
     }
 }
